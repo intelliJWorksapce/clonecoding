7401a30359d7d2844f12263a7af8453bdb526558
mgl <mgl0204@naver.com>
2023-04-26 23:52:43 +0900

    First Commit

---
+++ src/main/java/com/example/clonecoding/ClonecodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.clonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonecodingApplication.class, args);
+	}
+
+}
